eb24b8def0124fe104b308b57a4fe3b04fe7eb8c
codesboy <565195693@qq.com>
2017-08-24 23:41:20 +0800

    update

---
+++ application/api/controller/v1/Order.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\api\controller\v1;
+use think\Controller;
+//下单接口
+class Order extends Controller{
+    // 1.用户在选择好商品后，向API提交他选择的商品的相关信息
+    // 2.API在接收到提交来的商品数据时，需要检测订单所包含的商品的库存量
+
+}
