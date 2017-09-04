9dba48d5daed136057c33ab56f514eee31f30eda
codesboy <565195693@qq.com>
2017-09-04 17:59:45 +0800

    update

--- application/api/controller/v1/Pay.php
+++ application/api/controller/v1/Pay.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\api\controller\v1;
 use app\api\validate\IdPositiveInt;
-
+use app\api\service\Pay as PayService;
 // 支付接口
 class Pay extends Base{
     // 前置操作-验证权限
@@ -10,7 +10,9 @@ class Pay extends Base{
     // 生成预定单
     public function getPreOrder($id=''){
         (new IdPositiveInt())->doCheck();
+        $pay=new PayService($id);
 
+        $pay->pay();
     }
 
 
--- application/api/service/Pay.php
+++ application/api/service/Pay.php
@@ -8,11 +8,14 @@ use app\lib\exception\OrderException;
 use app\api\service\Token as TokenService;
 use app\lib\exception\TokenException;
 use app\lib\enum\OrderStatusEnum;
+use think\Loader;
 
+// Loader::import方法先导入没有使用命名空间的扩展类库  extend/WxPay/WxPay.Api.php
+Loader::import('WxPay.WxPay',EXTEND_PATH,'Api.php');
 
 class Pay{
     private $orderID;
-    private $orderNoO
+    private $orderNo;
 
     function __construct($orderID){
         if(!$orderID){
@@ -29,6 +32,8 @@ class Pay{
         if(!$status['pass']){
             return $status;// 库存量检测未通过
         }
+
+        return $this->makeWxPreOrder($status['orderPrice']);
     }
 
     // 通过订单id号检测该订单是否存在
@@ -54,7 +59,7 @@ class Pay{
                 'msg'=>'订单已经支付过了',
                 'ErrorCode'=>80003,
                 'code'=>400
-            ])
+            ]);
         }
 
 
@@ -64,7 +69,38 @@ class Pay{
 
 
     // 向微信服务器发送预定单请求
-    private function makeWxPreOrder(){
+    private function makeWxPreOrder($totalPrice){
+        $openid=TokenService::getCurrentTokenVal('openid');// 获得当前用户的openid
+        if(!$openid){
+            throw new TokenException();
+        }
 
+        $wxOrderData=new \WxPayUnifiedOrder();
+
+        $wxOrderData->SetOut_trade_no($this->orderNO);//设置订单编号
+        $wxOrderData->SetTrade_type('JSAPI');//设置订单类型
+        $wxOrderData->SetTotal_fee($totalPrice*100);//设置订单总价格
+        $wxOrderData->SetBody('零食商贩');
+        $wxOrderData->SetOpenid($openid);
+        $wxOrderData->SetNotify_url('http://qq.com');//
+
+        return $this->getPaySignature($wxOrderData);
+    }
+
+    private function getPaySignature($wxOrderData){
+        // 调用微信预订单接口
+        $wxOrder=new \WxPayApi;
+        dump($wxOrder);die;
+
+
+        // $wxOrder=new \WxPayApi::unifiedOrder($wxOrderData);
+        if($wxOrder['return_code'] != 'SUCCESS' || $wxOrder['result_code'] != 'SUCCESS'){
+
+            // 记录到日志
+            Log::record($wxOrder,'error');
+            Log::record('获取预支付订单失败','error');
+        }
+
+        return null;
     }
 }
